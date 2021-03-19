01c4010d475a8feb3d531cde9ae528b178e62157
Juan Sanchez <juan.sanchez@phila.gov>
2021-03-19 04:36:17 -0400

    changed name and role. added foreign keys and table selections.

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,16 @@ CREATE database employeesDB;
 USE employeesDB;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NULL,
+    department_name VARCHAR(30) NULL,
     PRIMARY KEY (id)
 );
-CREATE TABLE role (
+CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NULL,
     salary DECIMAL(10, 2) NULL,
     department_id INT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
@@ -19,5 +20,14 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NULL,
     role_id INT NULL,
     manager_id INT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
+SET FOREIGN_KEY_CHECKS = 0;
+SELECT *
+FROM employeesDB.department;
+SELECT *
+FROM employeesDB.employee;
+SELECT *
+FROM employeesDB.employee_role;
