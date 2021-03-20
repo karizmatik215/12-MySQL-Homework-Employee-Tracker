03a6ad62df0362bac898227621c47c364616b97a
Juan Sanchez <juan.sanchez@phila.gov>
2021-03-20 05:36:51 -0400

    added ascii message. updated view employees query. added view all departments and roles.

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ connection.connect((err) => {
     console.log(
       `Connected to Employee Tracker on port ${connection.config.port}`
     );
+    console.log(`
+  ---------------------------------------------------------------------------------------                                                
+ ________                          __                                         
+/        |                        /  |                                        
+$$$$$$$$/  _____  ____    ______  $$ |  ______   __    __   ______    ______  
+$$ |__    /     \/    \  /      \ $$ | /      \ /  |  /  | /      \  /      \ 
+$$    |   $$$$$$ $$$$  |/$$$$$$  |$$ |/$$$$$$  |$$ |  $$ |/$$$$$$  |/$$$$$$  |
+$$$$$/    $$ | $$ | $$ |$$ |  $$ |$$ |$$ |  $$ |$$ |  $$ |$$    $$ |$$    $$ |
+$$ |_____ $$ | $$ | $$ |$$ |__$$ |$$ |$$ \__$$ |$$ \__$$ |$$$$$$$$/ $$$$$$$$/ 
+$$       |$$ | $$ | $$ |$$    $$/ $$ |$$    $$/ $$    $$ |$$       |$$       |
+$$$$$$$$/ $$/  $$/  $$/ $$$$$$$/  $$/  $$$$$$/   $$$$$$$ | $$$$$$$/  $$$$$$$/ 
+                        $$ |                    /  \__$$ |                    
+ __       __            $$ |                    $$    $$/                     
+/  \     /  |           $$/                      $$$$$$/                      
+$$  \   /$$ |  ______   _______    ______    ______    ______    ______       
+$$$  \ /$$$ | /      \ /       \  /      \  /      \  /      \  /      \      
+$$$$  /$$$$ | $$$$$$  |$$$$$$$  | $$$$$$  |/$$$$$$  |/$$$$$$  |/$$$$$$  |     
+$$ $$ $$/$$ | /    $$ |$$ |  $$ | /    $$ |$$ |  $$ |$$    $$ |$$ |  $$/      
+$$ |$$$/ $$ |/$$$$$$$ |$$ |  $$ |/$$$$$$$ |$$ \__$$ |$$$$$$$$/ $$ |           
+$$ | $/  $$ |$$    $$ |$$ |  $$ |$$    $$ |$$    $$ |$$       |$$ |           
+$$/      $$/  $$$$$$$/ $$/   $$/  $$$$$$$/  $$$$$$$ | $$$$$$$/ $$/            
+                                           /  \__$$ |                         
+                                           $$    $$/                          
+                                            $$$$$$/                                                   
+-----------------------------------------------------------------------------------------
+  `);
   } else throw console.error('Error Connecting');
   start();
 });
@@ -58,18 +84,27 @@ const start = () => {
       }
     });
 };
-
+//View All Employees
 const viewAllEmployees = () => {
   connection.query(
-    `SELECT employee.id,
-      employee.first_name AS "First Name",
-      employee.last_name AS "Last Name",
-      employee_role.title AS "Role",
-      employee_role.salary AS "Salary",
-      department.department_name AS "Department"
-    FROM employee
-      INNER JOIN employee_role ON (employee_role.id = employee.role_id)
-      INNER JOIN department ON (department.id = employee_role.department_id)
+    `SELECT 
+      employee.id,
+      employee.first_name AS 'First Name',
+      employee.last_name AS 'Last Name',
+      employee_role.title AS 'Title',
+      employee_role.salary AS 'Salary',
+      department.department_name AS 'Department',
+      CONCAT(manager.first_name,
+            ' ',
+            manager.last_name) AS 'Manager'
+    FROM
+      employee
+        LEFT JOIN
+      employee_role ON (employee_role.id = employee.role_id)
+        LEFT JOIN
+      department ON (department.id = employee_role.department_id)
+        LEFT JOIN
+      employee manager ON manager.id = employee.manager_id
     ORDER BY employee.id;`,
     (error, query) => {
       if (error) throw error;
@@ -78,10 +113,39 @@ const viewAllEmployees = () => {
     }
   );
 };
-
+//Add An Employee
 const addEmployee = () => {};
-const viewDepartments = () => {};
+//View All Departments
+const viewDepartments = () => {
+  connection.query(
+    `SELECT
+      department_name AS Department
+    FROM 
+      department`,
+    (error, query) => {
+      if (error) throw error;
+      console.table(query);
+      start();
+    }
+  );
+};
+//Add A Department
 const addDepartment = () => {};
-const viewEmployeeRoles = () => {};
+//View All Employee Roles
+const viewEmployeeRoles = () => {
+  connection.query(
+    `SELECT 
+      title AS Title
+    FROM
+      employee_role`,
+    (error, query) => {
+      if (error) throw error;
+      console.table(query);
+      start();
+    }
+  );
+};
+//Add An Employee Role
 const addRole = () => {};
+//Update An Employee Role
 const updateEmployeeRole = () => {};
