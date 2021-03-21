2bd0bf0105aad1737fec319ebb6cb5762c987ee1
Juan Sanchez <juan.sanchez@phila.gov>
2021-03-20 21:13:30 -0400

    updated schema to include constraints

--- db/schema.sql
+++ db/schema.sql
@@ -3,16 +3,17 @@ CREATE database employeesDB;
 USE employeesDB;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    department_name VARCHAR(30) NULL,
+    department_name VARCHAR(30) UNIQUE NULL,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee_role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NULL,
+    title VARCHAR(30) UNIQUE NULL,
     salary DECIMAL(10, 2) NULL,
     department_id INT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    INDEX dep_ind (department_id),
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
 );
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
@@ -21,10 +22,12 @@ CREATE TABLE employee (
     role_id INT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
-    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    INDEX role_ind (role_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES employee_role (id) ON DELETE CASCADE,
+    INDEX man_ind (manager_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee (id) ON DELETE
+    SET NULL
 );
-SET FOREIGN_KEY_CHECKS = 0;
 SELECT *
 FROM employeesDB.department;
 SELECT *
